9666b32a6ea91910baa8222a75a43bc8e51569b3
yhotta240 <yhotta240@icloud.com>
2026-05-01 00:13:33 +0900

    chore: URL 変更時のデバッグログを削除

--- src/content/observer.ts
+++ src/content/observer.ts
@@ -289,7 +289,6 @@ export function createObserver(): MutationObserver {
     if (!customTab) {
       if (storageState.checkedTabs) handleFirstRender(elements, storageState.checkedTabs, isLargeScreen);
       if (preVideoId !== currentVideoId) {
-        console.log("URLが変更", storageState.preUrl, "から", url.href);
         handleUrlChange();
         storageState.preUrl = url.href;
       }
